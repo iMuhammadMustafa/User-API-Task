9a4bffd972799fe0d1868153583932c2a063e694
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2023-02-20 23:59:30 +0200

    Created Routes and Added Service Collection

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using User_API.Domain;
+using User_API.UserApi.Domain.DTOs;
+using User_API.UserApi.Services;
 using User_API.UserApi.Shared.Helpers;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddServices();
+builder.Services.AddServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -14,17 +18,30 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapPost("/user", () =>
-{
-    return "hello";
-})
-    .WithName("user");
 
-app.MapGet("/user", () =>
+//Task<ActionResult<NewUserResponseDTO>>
+
+app.MapPost("/user", AddUser).WithName("AddUser");
+app.MapGet("/user", GetUser).WithName("GetUser");
+
+static async Task<IResult> AddUser([FromBody] NewUserDTO newUser, [FromServices] IUserService _userService)
 {
-    return "hello";
-})
-    .WithName("user");
+    if (newUser == null || newUser.Email == null)
+    {
+        return Results.BadRequest();
+    }
+
+    var response = await _userService.AddUser(newUser);
+    return Results.Ok(response);
+}
+static async Task<IResult> GetUser([FromRoute] string id, [FromServices] IUserService _userService)
+{
+    if (id == null) return Results.BadRequest();
+    var response = await _userService.GetUserById(id);
+    if (response == null) return Results.NotFound();
+    return Results.Ok(response);
+}
+
 
 
 app.Run();
--- UserApi.Shared/Helpers/ServiceCollectionExtension.cs
+++ UserApi.Shared/Helpers/ServiceCollectionExtension.cs
@@ -1,11 +1,13 @@
-﻿using User_API.UserApi.Repository;
+﻿using Microsoft.EntityFrameworkCore;
+using User_API.Domain;
+using User_API.UserApi.Repository;
 using User_API.UserApi.Services;
 
 namespace User_API.UserApi.Shared.Helpers
 {
     public static class ServiceCollectionExtension
     {
-        public static IServiceCollection AddServices(this IServiceCollection services)
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
         {
             // Add services to the container.
             services.AddEndpointsApiExplorer();
@@ -15,7 +17,7 @@ namespace User_API.UserApi.Shared.Helpers
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             // Add Database Context
-            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("SaqayaUserApiDB")));
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("SaqayaUserApiDB")));
             //services.AddDatabaseDeveloperPageExceptionFilter();
 
 
