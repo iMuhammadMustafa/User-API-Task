50b5408939bc367e1931d82781e5165355cfee19
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2023-02-20 23:10:37 +0200

    Add User Repository

---
+++ UserApi.Repository/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using User_API.Domain;
+
+namespace User_API.UserApi.Repository
+{
+    public interface IUserRepository
+    {
+        public Task<User> GetUserById(string id);
+        public Task<User> AddUser(User newUser);
+    }
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UserRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<User> AddUser(User newUser)
+        {
+            await _appDbContext.Users.AddAsync(newUser);
+            await _appDbContext.SaveChangesAsync();
+
+            return newUser;
+        }
+
+        public async Task<User> GetUserById(string id)
+        {
+            return await _appDbContext.Users.FirstOrDefaultAsync(user => user.Id == id);
+        }
+    }
+}
