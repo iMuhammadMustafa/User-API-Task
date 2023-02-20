4438fad9d40c0793b340afeb2fd3743f44f1b146
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2023-02-20 22:46:32 +0200

    Add DTOs

---
+++ UserApi.Domain/DTOs/PostUserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace User_API.UserApi.Domain.DTOs
+{
+    public class PostUserDTO
+    {
+        public string Id { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
---
+++ UserApi.Domain/DTOs/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace User_API.UserApi.Domain.DTOs
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public bool MarketingConsent { get; set; }
+    }
+}
